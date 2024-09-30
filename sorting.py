f555a6faaaee82d70b626abab01150d7d083db6d
GavinR <riggigav000@alpinesd.org>
2024-09-30 14:07:32 -0600

    Update sorting.py
    - Added comments and docstring where I believed were needed
    - removed the ugly mess I used for testing

--- sorting.py
+++ sorting.py
@@ -1,32 +1,26 @@
+"""
+Gavin Riggins
+9/30/2024
+Advanced Programming & Data Structures
+
+This file contains an implimentation of the bubble sort search algorithm. 
+"""
+import random 
 
 def bubble(input_list):
+    """Sort via bubble sort"""
+    # Note: Two nested loops that loop the same thing feels wrong. 
     for e in range(0, len(input_list)):
 
-        for i in range(0, len(input_list) - e):
+        for i in range(0, len(input_list) - e): # e-1 ignores the previous searches
             if e != 0 and input_list[i + 1] < input_list[i]:
+                # Store i+1 so we could switch it with i without any problems
                 storage_var = input_list[i + 1]
                 input_list[i+1] = input_list[i]
                 input_list[i] = storage_var
 
+    # Return the sorted list for future use later
     return input_list    
 
-
-# print([5, 87, 3, 325, 2, 53, 23, 1, 63, 32, 12, 63, 23, 632])
-# print(bubble([5, 87, 3, 325, 2, 53, 23, 1, 63, 32, 12, 63, 23, 632]))
-# print("\n")
-
-# print([89, 59, 296, 6, 294, 49, 271, 228, 281, 135, 8, 175, 122, 190, 184])
-# print(bubble([89, 59, 296, 6, 294, 49, 271, 228, 281, 135, 8, 175, 122, 190, 184]))
-# print("\n")
-
-# print([89, 59, 296, 6, 294, 49, 271, 228, 281, 135, 8, 175, 122, 190, 184, ])
-# print(bubble([89, 59, 296, 6, 294, 49, 271, 228, 281, 135, 8, 175, 122, 190, 184]))
-# print("\n")
-
-# print([12, 252, 158, 111, 82, 124, 167, 116, 197, 66, 129, 249, 134, 74, 274])
-# print(bubble([12, 252, 158, 111, 82, 124, 167, 116, 197, 66, 129, 249, 134, 74, 274]))
-# print("\n")
-
-# print([12, 252, 158, 111, 82, 124, 167, 116, 197, 66, 129, 249, 134, 74, 274, ])
-# print(bubble([12, 252, 158, 111, 82, 124, 167, 116, 197, 66, 129, 249, 134, 74, 274, ]))
-# print("\n")
+print(bubble([random.randint(1,100) for x in range(random.randint(1,100))]))
+print(bubble([random.randint(1,100) for x in range(random.randint(1,100))]))
